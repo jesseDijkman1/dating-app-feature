76b7b6e5debb2f8798267acd7a7e8be2f5eab334
Jesse Dijkman <jesse.dijkman@hva.nl>
2020-05-30 13:52:52 +0200

    Included partials for DOC head and Footer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const path = require("path")
 
 // Process.env.port is used by heroku
 const PORT = process.env.PORT || 3000
@@ -6,7 +7,7 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 app.set("view engine", "ejs")
-app.set("views", "./templates")
+app.set("views", path.resolve(__dirname, "./templates"))
 
 app.get("/", (req, res) => {
   res.render("index")
