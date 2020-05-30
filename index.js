50d2fe2b0343bb4a83c79745d7b00388754e2269
Jesse Dijkman <jesse.dijkman@hva.nl>
2020-05-30 13:43:35 +0200

    Basic setup for EJS

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
+app.set("view engine", "ejs")
+app.set("views", "./templates")
+
 app.get("/", (req, res) => {
-  res.send("Hello World!")
+  res.render("index")
 })
 
 app.listen(PORT, () => {
