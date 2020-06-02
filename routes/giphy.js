40d020fcbc654a741e30d32bcfed7e851aebacd4
Jesse Dijkman <jesse.dijkman@hva.nl>
2020-06-03 00:25:41 +0200

    Added feature for searching giphies

--- routes/giphy.js
+++ routes/giphy.js
@@ -2,13 +2,37 @@ const axios = require("axios")
 const express = require("express")
 const router = express.Router()
 
-// These routes are used for retrieving data with JavaScript or othertools
-router.post("/trending", async (req, res) => {
-  const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_API_KEY}`
+// Middleware
+const { isAuthorized } = require("../middleware")
+
+// Return the trending giphies to the front end
+router.post("/trending", isAuthorized, async (req, res) => {
+  const URL = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_API_KEY}`
   try {
-    const response = await axios.get(url)
+    const response = await axios.get(URL)
 
     res.status(200).json(response.data)
+  } catch (error) {
+    res.status(400).send("Bad Request", error)
+  }
+})
+
+// Search giphy and redirect to accessible giphy overview page
+router.post("/search", isAuthorized, async (req, res) => {
+  const { matchId, userId, query } = req.body
+  const URL = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${query}`
+
+  try {
+    const response = await axios.get(URL)
+    const giphies = response.data.data.map((giphy) => ({
+      alt: giphy.title,
+      src: giphy.images.original.url,
+      id: giphy.id,
+    }))
+
+    const renderData = { matchId, userId, giphies }
+
+    res.status(200).render("giphy-overview", renderData)
   } catch (error) {
     res.status(400).send("Bad Request", error)
   }
