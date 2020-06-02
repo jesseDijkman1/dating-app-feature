d47cbaf782bf152ff5951fa32a5fd94fdb7798ea
Jesse Dijkman <jesse.dijkman@hva.nl>
2020-06-02 18:50:51 +0200

    Refactored giphy.js

--- routes/giphy.js
+++ routes/giphy.js
@@ -2,10 +2,16 @@ const axios = require("axios")
 const express = require("express")
 const router = express.Router()
 
-router.get("/giphy/trending", async (req, res) => {
+// These routes are used for retrieving data with JavaScript or othertools
+router.post("/trending", async (req, res) => {
   const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_API_KEY}`
+  try {
+    const response = await axios.get(url)
 
-  const response = await axios.get(url)
-
-  res.json(response.data)
+    res.status(200).json(response.data)
+  } catch (error) {
+    res.status(400).send("Bad Request", error)
+  }
 })
+
+module.exports = router
