3c945dc1b5a5c6490c552db34cb31a620ea277da
Jesse Dijkman <jesse.dijkman@hva.nl>
2020-06-03 02:05:21 +0200

    styled log in page

--- src/index.js
+++ src/index.js
@@ -1,70 +1,70 @@
-const giphyStartBtn = document.getElementById("giphy-open")
-const giphyOverviewContainer = document.getElementById("giphy-grid")
-const giphySearchInput = document.getElementById("giphy-search-input")
+// const giphyStartBtn = document.getElementById("giphy-open")
+// const giphyOverviewContainer = document.getElementById("giphy-grid")
+// const giphySearchInput = document.getElementById("giphy-search-input")
 
-// Event listeners
-giphyStartBtn.addEventListener("click", toggleGiphySection)
+// // Event listeners
+// giphyStartBtn.addEventListener("click", toggleGiphySection)
 
-// Giphy search
-function getTrendingGiphys() {
-  return new Promise((resolve, reject) => {
-    void (async function () {
-      try {
-        const response = await fetch("/giphy/trending")
-        const data = await response.json()
+// // Giphy search
+// function getTrendingGiphys() {
+//   return new Promise((resolve, reject) => {
+//     void (async function () {
+//       try {
+//         const response = await fetch("/giphy/trending")
+//         const data = await response.json()
 
-        resolve(data)
-      } catch (err) {
-        console.error(err, "Something went wrong on the server")
-        resolve([])
-      }
-    })()
-  })
-}
+//         resolve(data)
+//       } catch (err) {
+//         console.error(err, "Something went wrong on the server")
+//         resolve([])
+//       }
+//     })()
+//   })
+// }
 
-function giphyElement(imgSrc, giphyId, matchId, userId) {
-  return `
-    <form action="/message/giphy" method="POST">
-      <input type="hidden" name="giphySrc" value="${imgSrc}">
-      <input type="hidden" name="matchId" value="${matchId}">
-      <input type="hidden" name="userId" value="${userId}">
-      <img src="${imgSrc}">
+// function giphyElement(imgSrc, giphyId, matchId, userId) {
+//   return `
+//     <form action="/message/giphy" method="POST">
+//       <input type="hidden" name="giphySrc" value="${imgSrc}">
+//       <input type="hidden" name="matchId" value="${matchId}">
+//       <input type="hidden" name="userId" value="${userId}">
+//       <img src="${imgSrc}">
 
-      <button type="submit">Send giphy</button>
-    </form>
-  `
-}
+//       <button type="submit">Send giphy</button>
+//     </form>
+//   `
+// }
 
-// Open giphy section
-async function toggleGiphySection() {
-  const { data } = await getTrendingGiphys()
-  const matchId = giphyOverviewContainer.getAttribute("data-match-id")
-  const userId = giphyOverviewContainer.getAttribute("data-user-id")
+// // Open giphy section
+// async function toggleGiphySection() {
+//   const { data } = await getTrendingGiphys()
+//   const matchId = giphyOverviewContainer.getAttribute("data-match-id")
+//   const userId = giphyOverviewContainer.getAttribute("data-user-id")
 
-  data.forEach((giphy) => {
-    const previewUrl = giphy.images.preview_gif.url
-    const giphyId = giphy.id
+//   data.forEach((giphy) => {
+//     const previewUrl = giphy.images.preview_gif.url
+//     const giphyId = giphy.id
 
-    const giphyBlock = giphyElement(previewUrl, giphyId, matchId, userId)
+//     const giphyBlock = giphyElement(previewUrl, giphyId, matchId, userId)
 
-    giphyOverviewContainer.innerHTML += giphyBlock
-  })
+//     giphyOverviewContainer.innerHTML += giphyBlock
+//   })
 
-  // data.forEach((obj) => {
-  // const previewUrl =
+//   // data.forEach((obj) => {
+//   // const previewUrl =
 
-  // const giphyFormContainer = document.createElement("FORM")
-  // giphyFormContainer.action = "/message/giphy"
-  // giphy
+//   // const giphyFormContainer = document.createElement("FORM")
+//   // giphyFormContainer.action = "/message/giphy"
+//   // giphy
 
-  // const imgElement = document.createElement("IMG")
+//   // const imgElement = document.createElement("IMG")
 
-  // imgElement.src = previewUrl
+//   // imgElement.src = previewUrl
 
-  //   giphyOverviewContainer.appendChild(imgElement)
-  // })
-}
+//   //   giphyOverviewContainer.appendChild(imgElement)
+//   // })
+// }
 
-function updateOverview(html) {
-  // giphyOverviewContainer
-}
+// function updateOverview(html) {
+//   // giphyOverviewContainer
+// }
